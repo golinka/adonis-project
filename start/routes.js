e446a28079d181bcf34ec08b3132affa6a8e7ae6
Maxim Lyatsky <mlyatsky@mindk.com>
2018-12-17 19:00:51 +0200

    init

---
+++ start/routes.js
@@ -0,0 +1,17 @@
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URL's and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.0/routing
+|
+*/
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route');
+
+Route.get('/', () => ({ status: 'Ok', version: '1.0.0' }));

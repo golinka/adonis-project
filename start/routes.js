833207686aa2027950beed90f08edd3d0454872e
golinka <moyvebtop@gmail.com>
2018-12-21 14:56:20 +0200

    chore(routes): refactoring [prettier]

--- start/routes.js
+++ start/routes.js
@@ -17,21 +17,21 @@ const Route = use('Route');
 Route.get('/', () => ({ status: 'Ok', version: '1.0.0' }));
 
 Route.group(() => {
-    Route.get('/products', 'ProductController.index');
-    Route.post('/products', 'ProductController.store');
-    Route.get('/products/:pid', 'ProductController.show');
-    Route.put('/products/:pid', 'ProductController.update');
-    Route.delete('/products/:pid', 'ProductController.delete');
+  Route.get('/products', 'ProductController.index');
+  Route.post('/products', 'ProductController.store');
+  Route.get('/products/:pid', 'ProductController.show');
+  Route.put('/products/:pid', 'ProductController.update');
+  Route.delete('/products/:pid', 'ProductController.delete');
 
-    Route.get('/types', 'TypeController.index');
-    Route.post('/types', 'TypeController.store');
-    Route.get('/types/:tid', 'TypeController.show');
-    Route.put('/types/:tid', 'TypeController.update');
-    Route.delete('/types/:tid', 'TypeController.delete');
-    
-    Route.get('/types/:tid/fields', 'FieldController.index');
-    Route.post('/types/:tid/fields', 'FieldController.store');
-    Route.get('/types/:tid/fields/:fid', 'FieldController.show');
-    Route.put('/types/:tid/fields/:fid', 'FieldController.update');
-    Route.get('/types/:tid/fields/:fid/delete', 'FieldController.delete');
-}).prefix('api/v1')
+  Route.get('/types', 'TypeController.index');
+  Route.post('/types', 'TypeController.store');
+  Route.get('/types/:tid', 'TypeController.show');
+  Route.put('/types/:tid', 'TypeController.update');
+  Route.delete('/types/:tid', 'TypeController.delete');
+
+  Route.get('/types/:tid/fields', 'FieldController.index');
+  Route.post('/types/:tid/fields', 'FieldController.store');
+  Route.get('/types/:tid/fields/:fid', 'FieldController.show');
+  Route.put('/types/:tid/fields/:fid', 'FieldController.update');
+  Route.get('/types/:tid/fields/:fid/delete', 'FieldController.delete');
+}).prefix('api/v1');

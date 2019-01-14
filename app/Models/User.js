34fd998831cf242ae2e4ba553da99103ef8ba4d5
golinka <moyvebtop@gmail.com>
2019-01-15 00:01:48 +0200

    chore(models): hidden user password

--- app/Models/User.js
+++ app/Models/User.js
@@ -2,6 +2,10 @@
 const Model = use('Model');
 
 class User extends Model {
+  static get hidden() {
+    return ['password'];
+  }
+
   products() {
     return this.hasMany('App/Models/Product');
   }

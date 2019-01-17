f88220340af0d0f5c07a64e39b2fd6eab95335dd
golinka <artem.holinka@gmail.com>
2019-01-17 18:59:05 +0200

    chore(validators): refactored product validator

--- app/Validators/CheckProduct.js
+++ app/Validators/CheckProduct.js
@@ -3,12 +3,11 @@ const Type = use('App/Models/Type');
 
 const fieldsFn = async (data, field, message) => {
   // eslint-disable-next-line
-  const { type_id, fields } = data;
-  const type = await Type.findOrFail(type_id);
+  const { type_id: tid, fields } = data;
+  const type = await Type.findOrFail(tid);
   const { rows: typeFields } = await type.fields().fetch();
-  const fieldCount = fields.filter(fieldItem => typeof fieldItem !== 'undefined').length;
 
-  let fails = typeFields.length !== fieldCount;
+  let fails = typeFields.length !== fields.filter(fieldItem => typeof fieldItem !== 'undefined').length;
   typeFields.forEach(typeField => {
     fails = fails || typeof fields[typeField.id] === 'undefined';
   });

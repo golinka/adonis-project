b5ac429d9949696d3b2ac8a41543fdb0a4751b5b
golinka <artem.holinka@gmail.com>
2019-01-20 17:43:34 +0200

    feat(validators): added custom validation messages

--- app/Validators/CheckProduct.js
+++ app/Validators/CheckProduct.js
@@ -7,11 +7,15 @@ const fieldsFn = async (data, field, message) => {
   const type = await Type.findOrFail(tid);
   const { rows: typeFields } = await type.fields().fetch();
 
-  let fails = typeFields.length !== fields.filter(fieldItem => typeof fieldItem !== 'undefined').length;
+  let fails =
+    typeFields.length !== fields.filter(fieldItem => typeof fieldItem !== 'undefined').length
+      ? 'Not all fields are sent from this type'
+      : false;
+  if (fails) throw fails;
+
   typeFields.forEach(typeField => {
     fails = fails || typeof fields[typeField.id] === 'undefined';
   });
-
   if (fails) throw message;
 };
 
@@ -25,6 +29,14 @@ class CheckProduct {
       price: 'required',
       type_id: 'required',
       fields: 'required|fields'
+    };
+  }
+
+  get messages() {
+    return {
+      required: '{{ field }} is required',
+      max: '{{ field }} must be no longer than 255 characters',
+      fields: '{{ field }} must belong to product type and contain ID'
     };
   }
 }
--- app/Validators/CheckType.js
+++ app/Validators/CheckType.js
@@ -4,6 +4,14 @@ class CheckType {
       name: 'required|unique:types|max:30'
     };
   }
+
+  get messages() {
+    return {
+      required: '{{ field }} is required',
+      unique: '{{ field }} must be unique',
+      max: '{{ field }} must be no longer than 30 characters'
+    };
+  }
 }
 
 module.exports = CheckType;

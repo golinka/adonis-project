d97c4aac16fafb7e908872d3a460550658b462d9
golinka <artem.holinka@gmail.com>
2019-01-20 15:32:44 +0200

    feat(commands): added user data validation

--- app/Commands/CreateUser.js
+++ app/Commands/CreateUser.js
@@ -1,5 +1,7 @@
 const { Command } = require('@adonisjs/ace');
 
+const { validateAll } = use('Validator');
+
 const User = use('App/Models/User');
 const Role = use('App/Models/Role');
 const Database = use('Database');
@@ -14,13 +16,26 @@ class CreateUser extends Command {
   }
 
   async handle() {
+    const rules = {
+      username: 'required|unique:users|min:4|max:80',
+      email: 'required|unique:users|max:255|email',
+      password: 'required|min:6|max:12'
+    };
     const data = {
       username: await this.ask('Enter username:'),
       email: await this.ask('Enter email:'),
       password: await this.secure('Enter password:')
     };
-    const roleName = await this.choice('Select user role', ['admin', 'user'], 'user');
 
+    const validation = await validateAll(data, rules);
+    if (validation.fails()) {
+      validation.messages().forEach(error => {
+        this.error(`${this.icon('error')} Error: ${error.message}`);
+      });
+      process.exit(-1);
+    }
+
+    const roleName = await this.choice('Select user role', ['admin', 'user'], 'user');
     const role = await Role.query()
       .where('slug', roleName)
       .firstOrFail();
